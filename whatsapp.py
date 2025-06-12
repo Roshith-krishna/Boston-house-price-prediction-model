e1919034f2e9b71cc1b92fef6ab94b2aa719ce4d
Roshith-krishna <roshithkrishnakm.com>
2025-06-12 15:27:08 +0530

    initial commit

--- whatsapp.py
+++
@@ -1,6 +0,0 @@
-
-import pywhatkit as kit
-phone_number = "+919539628147"
-message = "Hello! This is a test message."
-for _ in range(5):  
-    kit.sendwhatmsg_instantly(phone_number, message, wait_time=5)
